bd56b0db647ca9fc444add37d71697f60028afe7
Hoo Di Heng <Hoodiheng@gmail.com>
2024-10-13 18:33:38 +0800

    added dependencies

---
+++ frontend/src/psalogo.js
@@ -0,0 +1,15 @@
+const Logo = () => {
+    return (
+      <img 
+        src="https://e7.pngegg.com/pngimages/303/452/png-clipart-logo-port-of-singapore-psa-international-global-container-terminal-text-logo.png" 
+        alt="Port of Singapore Logo" 
+        style={{ 
+          width: '100px', 
+          height: 'auto', 
+          animation: 'rotate 2s linear infinite' 
+        }} 
+      />
+    );
+  };
+
+export default Logo;
